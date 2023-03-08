e776e769e3a4f90fcf375538e592fd4fc9512ea3
seanjaffe1 <seanjaffe1@gmail.com>
2023-03-08 17:14:38 +0000

    updated test

--- projects/final-project/tests/q1_1_3.py
+++ projects/final-project/tests/q1_1_3.py
@@ -2,7 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q1_1_3',
     'points': [0, 0],
-    'suites': [   {   'cases': [   {'code': '>>> print(type(num_unique_cities) in set([int, np.int8, np.int16, np.int32, np.int64]))\n>>> \nTrue\n', 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {'code': '>>> type(num_unique_cities) in set([int, np.int8, np.int16, np.int32, np.int64])\nTrue', 'hidden': False, 'locked': False},
                                    {'code': '>>> # Make sure that num_unique_cities is greater than zero!\n>>> num_unique_cities > 0\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
